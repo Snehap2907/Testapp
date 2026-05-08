3b37ab2c69a76a192f50ecea655ef955ca296234
Snehap2907 <Snehapandey.sp.14@gmail.com>
2026-05-09 02:36:34 +0530

    staged file commited

--- Flaskapp.py
+++ Flaskapp.py
@@ -2,5 +2,10 @@ print('flask is used to develop API')
 print("web API1")
 print("web API2")
 print("web API3")
-print(""write 1)
-print(""write 2)
+print("write 1")
+print("write 2")
+# i=1
+# for i in i<=5:
+#     print("Git Staging test")
+# i=i+1
+print('test 12')
