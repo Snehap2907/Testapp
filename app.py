35de1e7e64158c1f523cf4277cca74d61f8ba11f
Snehap2907 <snehapandey.sp.14@gmail.com>
2026-05-15 13:07:57 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-print('Testinng Branch in this section')
-print('Will check master and branch')
-print('staging new changes')
-<<<<<<< HEAD
-print('check if changes applied')
-print('merging branch')
-=======
-print('check if changes applied')
->>>>>>> branch11
+print('Enter a feature')
+print('Enter second feature')
+print('Enter third feature')
